d78d83bfdcc95d941bb9b202df464eaf9be67bc4
juanf1328 <jforni@encodesa.com.ar>
2023-06-27 14:07:52 -0300

    second push npx

--- rsh/src/App.js
+++ rsh/src/App.js
@@ -1,9 +1,11 @@
+import { Button } from 'bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>hellooooooooooo</h2>
+      <h2 className="title">Welcome</h2>
+      <button className='btn'>Open View</button>
     </div>
   );
 }
